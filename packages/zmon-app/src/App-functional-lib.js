c9f1057863c27f717fb88398dcce916dc2a23d19
eunbeen-ahn <aeb@sk.com>
2020-06-17 12:55:34 +0900

    chore: rename packages

---
+++ packages/zmon-app/src/App-functional-lib.js
@@ -0,0 +1,20 @@
+import React, { useState, useRef, useEffect } from "react";
+import "./styles.css";
+import Header from "./components/Header"
+import { getGNB } from 'zmon-ui' // zmon 프로젝트에 포함시킬때
+
+
+export default function App() {
+  const gnbEl = useRef(null);
+  useEffect(props => {
+    getGNB(gnbEl.current)
+  }, []);
+
+  return (
+    <div className="App">
+      <div id="gnb" ref={gnbEl}/>
+      <Header/>
+      <h2>Functional Component</h2>
+    </div>
+  );
+}
--- packages/zmon-app/src/App-functional.js
+++ packages/zmon-app/src/App-functional.js
@@ -1,19 +1,11 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./styles.css";
-import Header from "./components/Header"
-import { getGNB } from 'zmon-gnb' // zmon 프로젝트에 포함시킬때
-
+import { GNB } from "@zmon/ui-react"
 
 export default function App() {
-  const gnbEl = useRef(null);
-  useEffect(props => {
-    getGNB(gnbEl.current)
-  }, []);
-
   return (
     <div className="App">
-      <div id="gnb" ref={gnbEl}/>
-      <Header/>
+      <GNB/>
       <h2>Functional Component</h2>
     </div>
   );
